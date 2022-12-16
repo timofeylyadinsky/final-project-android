158585163381c81c2917e14faa23e2a197760c35
Timofey Lyadinsky <tima.led12@gmail.com>
2022-12-17 01:44:01 +0300

    feat: add splash screen

---
+++ app/src/main/java/lt/timofey/finalprojectandroid/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package lt.timofey.finalprojectandroid
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        iv_note.alpha = 0f
+        iv_note.animate().setDuration(1500).alpha(1f).withEndAction(){
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
