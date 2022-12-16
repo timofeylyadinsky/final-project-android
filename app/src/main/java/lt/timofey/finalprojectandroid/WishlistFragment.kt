b21ae3469d0efcaf9781e8eb0db8c73a82a0d398
Timofey Lyadinsky <tima.led12@gmail.com>
2022-12-16 20:29:12 +0300

    feat: add second fragment

---
+++ app/src/main/java/lt/timofey/finalprojectandroid/WishlistFragment.kt
@@ -0,0 +1,58 @@
+package lt.timofey.finalprojectandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import lt.timofey.finalprojectandroid.adapters.WishlistAdapter
+import lt.timofey.finalprojectandroid.db.Car
+import lt.timofey.finalprojectandroid.db.CarsWishlistDB
+
+
+class WishlistFragment : Fragment() {
+    var carsDB = listOf<Car>()
+    val carDBList = mutableListOf<Car>()
+    lateinit var carsWishlistRecyclerViewList: RecyclerView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wishlist, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val db = Room.databaseBuilder(
+            requireActivity().applicationContext,
+            CarsWishlistDB::class.java, "car_db"
+        ).allowMainThreadQueries()//Делаем в отдельном потоке, в данном случае в главном потоке
+            .build()
+
+        val carWishlistDao = db.carWishlistDao()
+
+        carsDB = carWishlistDao.getAll()
+        carsWishlistRecyclerViewList = requireActivity().findViewById(R.id.recyclerViewWishlist)
+        carsWishlistRecyclerViewList.layoutManager = LinearLayoutManager(requireActivity())
+        carsWishlistRecyclerViewList.adapter = WishlistAdapter(carsDB,this@WishlistFragment)
+    }
+    fun deleteItemFromWishList(position: Int){
+        val db = Room.databaseBuilder(
+            requireContext().applicationContext,
+            CarsWishlistDB::class.java, "car_db"
+        ).allowMainThreadQueries()//Делаем в отдельном потоке, в данном случае в главном потоке
+            .build()
+
+        val carWishlistDao = db.carWishlistDao()
+
+        val expectedCars = carWishlistDao.findByPrimaryKey(carsDB[position].id)
+        carWishlistDao.deleteByName(expectedCars[0].id)
+
+        carsDB = carWishlistDao.getAll()
+        carsWishlistRecyclerViewList = requireActivity().findViewById(R.id.recyclerViewWishlist)
+        carsWishlistRecyclerViewList.layoutManager = LinearLayoutManager(requireActivity())
+        carsWishlistRecyclerViewList.adapter = WishlistAdapter(carsDB,this@WishlistFragment)
+    }
+}
---
+++ app/src/main/java/lt/timofey/finalprojectandroid/adapters/WishlistAdapter.kt
@@ -0,0 +1,49 @@
+package lt.timofey.finalprojectandroid.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import lt.timofey.finalprojectandroid.R
+import lt.timofey.finalprojectandroid.WishlistFragment
+import lt.timofey.finalprojectandroid.db.Car
+
+class WishlistAdapter(val plist: List<Car>, val wactivity: WishlistFragment): RecyclerView.Adapter<WishlistAdapter.WishlistViewHolder>() {
+
+    class WishlistViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WishlistViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        var holder: WishlistAdapter.WishlistViewHolder //= PersonViewHolder(inflater.inflate(R.layout.item_layout, parent, false))
+        holder = WishlistViewHolder(inflater.inflate(R.layout.wishlist_item_layout, parent, false))
+
+        /*holder.itemView.setOnClickListener{
+            mactivity.itemClick(holder.adapterPosition)
+        }*/
+        val btnDel = holder.itemView.findViewById<ImageButton>(R.id.deleteFromFavourite)
+
+        btnDel.setOnClickListener {
+            wactivity.deleteItemFromWishList(holder.adapterPosition)
+        }
+        /*holder.itemView.setOnLongClickListener {
+            mactivity.removeItem(holder.adapterPosition)
+            return@setOnLongClickListener true
+        }*/
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: WishlistViewHolder, position: Int) {
+        val carMaker = holder.itemView.findViewById<TextView>(R.id.carMaker)
+        carMaker.text = plist[position].maker +" "+ plist[position].model
+    }
+
+    override fun getItemCount(): Int {
+        return plist.count()
+    }
+}
