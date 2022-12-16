57f01882beeeeaabe17efe0c36ce390d2eace114
Timofey Lyadinsky <tima.led12@gmail.com>
2022-12-16 19:59:45 +0300

    feat: some changes

--- app/src/main/java/lt/timofey/finalprojectandroid/MainFragment.kt
+++ app/src/main/java/lt/timofey/finalprojectandroid/MainFragment.kt
@@ -1,6 +1,8 @@
 package lt.timofey.finalprojectandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +11,25 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import kotlinx.android.synthetic.main.item_layout.*
+import lt.timofey.finalprojectandroid.adapters.CarAdapter
+import lt.timofey.finalprojectandroid.api.ApiService
+import lt.timofey.finalprojectandroid.db.Car
+import lt.timofey.finalprojectandroid.db.CarsWishlistDB
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainFragment : Fragment() {
-
+    val apiService = ApiService.create()
     lateinit var navController: NavController
+    var cars = listOf<Car>()
+    val carList = mutableListOf<Car>()
+    lateinit var carsRecyclerViewList: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +41,60 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getCars()
+
+        /*addToFavourite.setOnClickListener(){
+            val intent = Intent(this, WishlistActivity::class.java)
+            startActivity(intent)
+        }*/
+    }
+    fun addItemToWishList(position: Int){
+        val db = Room.databaseBuilder(
+            requireContext().applicationContext,
+            CarsWishlistDB::class.java, "car_db"
+        ).allowMainThreadQueries()//Делаем в отдельном потоке, в данном случае в главном потоке
+            .build()
+
+        val carWishlistDao = db.carWishlistDao()
+
+        if (findById(cars[position].id)) {
+            carWishlistDao.addNew(cars[position])
+        }
+
+
+    }
+
+    fun findById(num: Int): Boolean{
+        val db = Room.databaseBuilder(
+            requireContext().applicationContext,
+            CarsWishlistDB::class.java, "car_db"
+        ).allowMainThreadQueries()//Делаем в отдельном потоке, в данном случае в главном потоке
+            .build()
+
+        val carWishlistDao = db.carWishlistDao()
+
+        val expectedCarsFromDB = carWishlistDao.findByPrimaryKey(num)
+
+        return expectedCarsFromDB.isEmpty()
+    }
+
+    fun getCars(){
+        val call = apiService.getCars()
+
+        call.enqueue(object: Callback<List<Car>> {
+
+            override fun onFailure(call: Call<List<Car>>, t: Throwable) {
+                Log.d("!!!",t.toString())
+            }
+
+            override fun onResponse(call: Call<List<Car>>, response: Response<List<Car>>) {
+                cars = response.body()!!
+                //tv.text = cars.toString()
+                carsRecyclerViewList = requireActivity().findViewById(R.id.recyclerView)
+                carsRecyclerViewList.layoutManager = LinearLayoutManager(requireContext())
+                carsRecyclerViewList.adapter = CarAdapter(cars,this@MainFragment)
+            }
+        })
     }
 
 }
---
+++ app/src/main/java/lt/timofey/finalprojectandroid/adapters/CarAdapter.kt
@@ -0,0 +1,45 @@
+package lt.timofey.finalprojectandroid.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import lt.timofey.finalprojectandroid.MainFragment
+import lt.timofey.finalprojectandroid.R
+import lt.timofey.finalprojectandroid.db.Car
+
+class CarAdapter(val plist: List<Car>, val mactivity: MainFragment): RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
+
+    class CarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        var holder: CarAdapter.CarViewHolder
+        holder = CarViewHolder(inflater.inflate(R.layout.item_layout, parent, false))
+
+        holder.itemView.setOnClickListener{
+            //mactivity.itemClick(holder.adapterPosition)
+        }
+        val btnAdd = holder.itemView.findViewById<ImageButton>(R.id.addToFavourite)
+
+        btnAdd.setOnClickListener {
+            mactivity.addItemToWishList(holder.adapterPosition)
+        }
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+        val carMaker = holder.itemView.findViewById<TextView>(R.id.carMaker)
+        carMaker.text = plist[position].maker +" "+ plist[position].model
+    }
+
+    override fun getItemCount(): Int {
+        return plist.count()
+    }
+}
